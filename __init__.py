1cafb2f42c0962b2c1ee267845c8a720acdb81c9
bookworm-coding <bookwormkkm00@gmail.com>
2023-08-28 19:01:33 +0900

    initial release

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
-from Zerogame.module import *
+from module import *
 
 if __name__ == "__main__":
-    print(Zero(int(input("제로게임 인원수를 입력하세요: "))))
+    number = int(input("제로게임 인원수를 입력하세요: "))
+    select = input("엑셀 파일을 원하면 E를, 텍스트 파일을 원하면 T를, 단순 출력을 원하면 P를 입력하세요")
+    if select == "E":
+        Zero(number).to_excel()
+    elif select == "T":
+        f = open("result.txt", "w")
+        print(Zero(number), file=f)
+    elif select == "P":
+        print(Zero(number))
+    else:
+        print("잘못된 입력입니다.")
