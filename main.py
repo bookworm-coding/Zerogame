20ddff417ebd7268a63668a244cb15b166773408
bookworm-coding <bookwormkkm00@gmail.com>
2023-09-05 16:41:05 +0900

    add .gitignore and integrate __init__.py and module.py into main.py

--- main.py
+++ main.py
@@ -45,3 +45,17 @@ class Zero(DataFrame):
 
     def result(self):
         return self.l
+
+
+if __name__ == "__main__":
+    number = int(input("제로게임 인원수를 입력하세요: "))
+    select = input("엑셀 파일을 원하면 E를, 텍스트 파일을 원하면 T를, 단순 출력을 원하면 P를 입력하세요")
+    if select == "E":
+        Zero(number).to_excel("result.xlsx")
+    elif select == "T":
+        f = open("result.txt", "w")
+        print(Zero(number), file=f)
+    elif select == "P":
+        print(Zero(number))
+    else:
+        print("잘못된 입력입니다. 다시 실행해주세요.")
